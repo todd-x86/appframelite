7ad745d83e52365d1bb8e1a3fa89b0942edad5af
Todd Suess <todd@datacomponents.net>
2014-11-16 10:05:47 -0500

    Added PHTML template adapter -- refactoring needed

--- lib/Render/PHTML.class.php
+++ lib/Render/PHTML.class.php
@@ -8,6 +8,9 @@
 
 namespace Base\Render;
 use Base\Render;
+use Base\Filter;
+use Base\Path;
+use Base\MVC\View;
 
 class PHTML extends Render
 {
@@ -23,5 +26,51 @@ class PHTML extends Render
 	{
 		extract($this->data, EXTR_SKIP);
 		require($_file);
+	}
+	
+	function w ($text)
+	{
+		print Filter::html($text);
+	}
+	
+	protected function content ()
+	{
+		$this->renderPHTML($this->path.'/'.$this->file.'.phtml');
+	}
+	
+	protected function theme ($text)
+	{
+		return Path::theme($text);
+	}
+	
+	protected function url ($text)
+	{
+		return Path::web($text);
+	}
+	
+	protected function date ($fmt, $timestamp)
+	{
+		return date($fmt, $timestamp);
+	}
+	
+	// [Template Function]
+	// Returns a human-readable file size
+	protected function filesize ($size)
+	{
+		return Filter::fileSize($size);
+	}
+	
+	// [Template Function]
+	// Returns true if a flash message was set
+	protected function hasFlash ()
+	{
+		return View::hasFlash();
+	}
+	
+	// [Template Function]
+	// Returns the flash message
+	protected function flash ()
+	{
+		return View::getFlash();
 	}
 }
